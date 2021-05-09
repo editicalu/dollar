db748b08fad578eb7b10da4108a702d8fc90460d
YTG123 <54103478+YTG1234@users.noreply.github.com>
2021-05-09 23:11:55 +0300

    Missed that

--- $.sh
+++ $.sh
@@ -4,7 +4,7 @@ VERSION=2
 if [ -z "$1" ]; then
 	echo "$ version ${VERSION}"
 	echo
-	echo "Usage: $ COMMAND [ARGS...]"
+	echo "Usage: $ COMMAND [ARGS..]"
 
 	# printf "$ version %s\n\nUsage: $ COMMAND [ARGS...]\n" "$VERSION"
 else
